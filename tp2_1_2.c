3f4600e1283fb09240d2c98881c2560f0b4c53da
“SueldoMariano” <sueldo.011.ms@gmail.com>
2023-03-28 19:07:58 -0300

    tp2_1_2.c agregado en la rama Option_2

---
+++ tp2_1_2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 20
+
+
+int main(){
+
+    int i;
+    double vt[N];
+    double *p = vt;
+
+    for(i = 0;i<N; i++)
+    {
+        *p = 1+rand()%100;
+        printf("%f ", *p);
+        p++;
+    }
+
+
+}
