987cc99037557297d3c05c7bdbae507fe88c049b
“SueldoMariano” <sueldo.011.ms@gmail.com>
2023-03-28 18:45:26 -0300

    se agrego tp2_1_1.c y se completo el codigo. Se agrego .gitignore

---
+++ tp2_1_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 20
+
+int i;
+double vt[N];
+int main(){
+
+    for(i = 0;i<N; i++)
+    {
+        vt[i]=1+rand()%100;
+        printf("%f ", vt[i]);
+    }
+
+
+}
